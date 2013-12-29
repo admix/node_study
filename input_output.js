1909e58ae52e1421d5ec8f0da44fd96af45054d3
admix <admix.snurnikov@gmail.com>
2013-12-28 23:31:17 -0500

    modified new line counter (input_output.js), added easy way to count new lines V. 2

--- input_output.js
+++ input_output.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var file = fs.readFileSync(process.argv[2],'utf8'); //reading the file and storing in the variable
+var newLines_easy = file.toString().split('\n').length - 1;
 var newLines = 0;
 for(var i = 0;i<file.length;i++){
 	if(file[i]=='\n'){
@@ -7,4 +8,5 @@ for(var i = 0;i<file.length;i++){
 	}
 }
 //console.log(file);
-console.log(newLines);
+console.log("with loop: "+newLines);
+console.log("easy: "+newLines_easy);
