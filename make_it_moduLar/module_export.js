bc56b6e000ebe73aa8d394dfe1ce271e05aa5d23
admix <admix.snurnikov@gmail.com>
2013-12-30 23:50:49 -0500

    added testing example and 2 argument program

--- make_it_moduLar/module_export.js
+++ make_it_moduLar/module_export.js
@@ -1,14 +1,17 @@
 //creating modules
-module.exports = function (dirName, extName, function (err, list) { //perhaps NOT NEEDED
-	if(err) throw err;
 
-	var result = Array();
-	var j = 0;
-	for(var i = 0;i<list.length;i++){
-		if (path.extname(list[i]) === extName){ //checking the extension
-			result[j] = list[i];
-			console.log(result[j]); //printing to console "one per line" the filtered files
-			j++;
-		}
-	}
-});
+exports.exp = function (dirName, extName){
+	var ret = dirName + extName;
+	return ret;
+};
+
+
+//testing the modules
+exports.test = function (animal){
+	var an = animal;
+	return an;
+}
+
+
+//module.exports = test;
+//module.exports = exp; //exporting the module
--- make_it_moduLar/module_program.js
+++ make_it_moduLar/module_program.js
@@ -2,12 +2,23 @@
 
 var fs = require('fs');
 var path = require('path');
-var mymodule = require('./module_exercise.js') //loading module to use it in this program
+var mymodule = require('./module_export') //loading module to use it in this program
+
 
 //assigning arguments to the values
 var dirName = process.argv[2];
 var extName = "."+process.argv[3]; //adding '.' to the provided extension
 
+console.log(mymodule.exp(dirName, extName)); //need to add 3rd argument
+
+console.log(mymodule.test("cat")); //testing the modules
+
+
+
+
+
+
+/*
 fs.readdir(dirName,function (err, list){ //perhaps NOT NEEDED
 	if(err) throw err;
 
@@ -21,3 +32,4 @@ fs.readdir(dirName,function (err, list){ //perhaps NOT NEEDED
 		}
 	}
 });
+*/
