0917616ea8c63e57ca838e25a6fa7b64abbe3931
admix <admix.snurnikov@gmail.com>
2014-01-01 21:45:37 -0500

    completed http_client program

--- http_client/http_client.js
+++ http_client/http_client.js
@@ -1,1 +1,36 @@
 //program to work with the client side 
+var fs = require('fs'); 
+var http = require('http'); //http core module
+var site = process.argv[2]; //provided website
+
+http.get(site,function (res) { //response
+	// body...
+	
+	//console.log("got response: " + res.statusCode); //getting status code
+	res.setEncoding('utf8');
+	res.on('data', function (data) { //fun callback
+		// body...
+		console.log('' + data);
+	})
+	res.on('error', console.error)
+})/*.on('data', function (data) {
+	// body...
+	console.log("Got data: " + data);
+}).on('error', function (e) { //error
+	// body...
+	console.log("Got error: " + e.message);
+})*/
+
+
+
+
+/* //easy asnwer
+var http = require('http')
+
+      http.get(process.argv[2], function (response) {
+        response.setEncoding('utf8')
+        response.on('data', console.log)
+        response.on('error', console.error)
+      })
+*/
+
