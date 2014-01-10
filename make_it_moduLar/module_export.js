4ec3d03a837bd1cc8b14f975db812ab379ab809b
admix <admix.snurnikov@gmail.com>
2014-01-10 00:38:13 -0500

    Make it modular - passed

--- make_it_moduLar/module_export.js
+++ make_it_moduLar/module_export.js
@@ -1,22 +1,45 @@
 //creating modules
 var path = require('path');
 var fs = require('fs');
+var result = [];
+var j = 0;
 
 module.exports = function (dirName, extName, callback) {
 	fs.readdir(dirName, function (err, list){
-		if(err) return callback(err);
-		else{
-			var result = [];
-			var j = 0;
+		
+		if(err){
+			return callback(err);
+		}else{
+			//result = list;
 			for(var i = 0;i<list.length;i++){
 				if (path.extname(list[i]) === extName){ //checking the extension
-					result[j] = list[i];
-					console.log(result[j]); //printing to console "one per line" the filtered files
+					list[j] = list[i];
+					console.log(list[j]); //printing to console "one per line" the filtered files
 					j++;
+				}else{
+
+					list.splice(3);
+
 				}
+
 			}
-			return callback(null,result);
+			//console.log(result);
 		}
+		callback(null,list);
+		
+		/*
+		if(err) return callback(err);
+		list.forEach (function (file) {
+			// body...
+			if (path.extname(file) === '.' + process.argv[3]){
+				console.log(file);
+			}else{
+				list.splice(3);
+			}
+		});
+		callback(null,list);
+		*/
+
 	  //easy way
 		/*list.forEach (function (file) {
 			// body...
