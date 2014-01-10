0a7851beab88c9376983db529cd46957c41b574c
admix <admix.snurnikov@gmail.com>
2014-01-10 00:02:30 -0500

    works fine, only problem is: 'doesn't return the correct number of elements as the second argument of the callback'

--- make_it_moduLar/module_export.js
+++ make_it_moduLar/module_export.js
@@ -2,36 +2,32 @@
 var path = require('path');
 var fs = require('fs');
 
-module.exports.exp = function (dirName, extName) {
-fs.readdir(dirName, function (err, list){
-	if(err) throw err;
-	var result = Array();
-	var j = 0;
-	for(var i = 0;i<list.length;i++){
-		if (path.extname(list[i]) === extName){ //checking the extension
-			result[j] = list[i];
-			console.log(result[j]); //printing to console "one per line" the filtered files
-			j++;
+module.exports = function (dirName, extName, callback) {
+	fs.readdir(dirName, function (err, list){
+		if(err) return callback(err);
+		else{
+			var result = [];
+			var j = 0;
+			for(var i = 0;i<list.length;i++){
+				if (path.extname(list[i]) === extName){ //checking the extension
+					result[j] = list[i];
+					console.log(result[j]); //printing to console "one per line" the filtered files
+					j++;
+				}
+			}
+			return callback(null,result);
 		}
-	}
-
-  //easy way
-	/*list.forEach (function (file) {
-		// body...
-		if (path.extname(file) === '.' + process.argv[3]){
-			console.log(file);
-		}
-	})*/
-});
+	  //easy way
+		/*list.forEach (function (file) {
+			// body...
+			if (path.extname(file) === '.' + process.argv[3]){
+				console.log(file);
+			}
+		})*/
+	});
 };
 
 //testing the modules
-function Test (animal){
-	var an = animal;
-	return an;
-}
-exports.Test = Test;
-
 
 //module.exports = test;
 //module.exports = exp; //exporting the module
--- make_it_moduLar/module_program.js
+++ make_it_moduLar/module_program.js
@@ -11,26 +11,16 @@ var extName = "."+process.argv[3]; //adding '.' to the provided extension
 
 //console.log(dirName);
 //console.log(mymodule.exp(dirName, extName)); //need to add 3rd argument
-mymodule.exp(dirName, extName);
+mymodule(dirName,extName);
 //console.log(mymodule.Test("kitten")); //testing the modules
 
+/*//callback example
 
+function doCallback(callback) {
+   callback()
+}
 
-
-
-
-/*
-fs.readdir(dirName,function (err, list){ //perhaps NOT NEEDED
-	if(err) throw err;
-
-	var result = Array();
-	var j = 0;
-	for(var i = 0;i<list.length;i++){
-		if (path.extname(list[i]) === extName){ //checking the extension
-			result[j] = list[i];
-			console.log(result[j]); //printing to console "one per line" the filtered files
-			j++;
-		}
-	}
-});
+doCallback(function() {
+   console.log('callback done')
+})
 */
