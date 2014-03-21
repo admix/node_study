e0ad16f1468984f0373c63f821f2506e179a96c5
admix <admix.snurnikov@gmail.com>
2014-03-21 00:26:55 -0400

    Completed time_server task

--- time_server/time_server.js
+++ time_server/time_server.js
@@ -16,3 +16,42 @@ var net = require('net'),
     bl = require('bl'),
     http = require('http'), //http core module
     site = process.argv[2]; //provided website
+
+var date = new Date(),
+    year = date.getFullYear(),
+    month = date.getMonth() + 1,
+    day = date.getDate(),
+    hour = date.getHours(),
+    min = date.getMinutes(),
+    finalDate = year + "-0" + month + "-" + day + " 0" + hour +":"+min;
+
+var server = net.createServer(function(connection) {
+  connection.write(finalDate + "\n");
+  connection.end();
+  //connection.end(data);
+})
+server.listen(8000);
+
+
+//Best Solution
+/*
+var net = require('net')
+
+function zeroFill(i) {
+  return (i < 10 ? '0' : '') + i
+}
+
+function now () {
+  var d = new Date()
+  return d.getFullYear() + '-'
+    + zeroFill(d.getMonth() + 1) + '-'
+    + zeroFill(d.getDate()) + ' '
+    + zeroFill(d.getHours()) + ':'
+    + zeroFill(d.getMinutes())
+}
+
+var server = net.createServer(function (socket) {
+  socket.end(now() + '\n')
+})
+server.listen(8001)
+*/
