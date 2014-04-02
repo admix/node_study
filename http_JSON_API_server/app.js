e25d3306299bbe53eb4afeecc96fbd9650dcd856
admix <admix.snurnikov@gmail.com>
2014-04-01 22:33:51 -0400

    Completed JSON API task

--- http_JSON_API_server/app.js
+++ http_JSON_API_server/app.js
@@ -30,11 +30,33 @@ var fs = require('fs'),
 
 http.createServer(function(req, res) {
 
-  var pathname = url.parse(req.url).pathname;
-  console.log("Request for " + pathname + " received.");
+  var query = url.parse(req.url, true);
+  var path = query['pathname'].split('/');
+  var date = new Date(query.query.iso);
+  console.log(date.getTime());
+  //console.log("Request for " + pathname + " received.");
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write("../README.md");
-  res.end();
+  if(path[2] == 'parsetime'){
+    var parsetime = {
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds()
+    };
+
+    res.write(JSON.stringify(parsetime));
+    res.end();
+
+  } else if(path[2] == 'unixtime'){
+      var unixtime = {
+      unixtime: date.getTime()
+    }
+
+    res.write(JSON.stringify(unixtime));
+    res.end();
+
+  } else{
+      res.end("404 error");
+  }
 
 }).listen(port, function () {
   // body...
@@ -43,5 +65,38 @@ http.createServer(function(req, res) {
 
 //Best Solution
 /*
+    var http = require('http')
+    var url = require('url')
 
+    function parsetime (time) {
+      return {
+        hour: time.getHours(),
+        minute: time.getMinutes(),
+        second: time.getSeconds()
+      }
+    }
+
+    function unixtime (time) {
+      return { unixtime : time.getTime() }
+    }
+
+    var server = http.createServer(function (req, res) {
+      var parsedUrl = url.parse(req.url, true)
+      var time = new Date(parsedUrl.query.iso)
+      var result
+
+      if (/^\/api\/parsetime/.test(req.url))
+        result = parsetime(time)
+      else if (/^\/api\/unixtime/.test(req.url))
+        result = unixtime(time)
+
+      if (result) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(result))
+      } else {
+        res.writeHead(404)
+        res.end()
+      }
+    })
+    server.listen(Number(process.argv[2]))
 */
