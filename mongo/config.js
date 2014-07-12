b6401f6ea9a0a09f961a7b91dda0b48d3bdf40f6
admix <admix.snurnikov@gmail.com>
2014-07-12 19:57:11 -0400

    Fixed configurations to run replica set locally

--- mongo/config.js
+++ mongo/config.js
@@ -1,7 +1,7 @@
 config = { _id: "rs1", members:[
-  { _id: 0, host: "admix.local:27017", priority:0, slaveDelay:5 },
-  { _id: 1, host: "admix.local:27018" },
-  { _id: 2, host: "admix.local:27019" }
+  { _id: 0, host: "localhost:27017", priority:0, slaveDelay:5 },
+  { _id: 1, host: "localhost:27018" },
+  { _id: 2, host: "localhost:27019" }
 ]}
 
 rs.initiate(config)
--- mongo/run_repl.sh
+++ mongo/run_repl.sh
@@ -1,3 +1,3 @@
-mongod --replSet rs1 --logpath "1.log" --dbpath ~/Sites/data/rs1 --port 27019 --fork
-mongod --replSet rs1 --logpath "2.log" --dbpath ~/Sites/data/rs2 --port 27019 --fork
+mongod --replSet rs1 --logpath "1.log" --dbpath ~/Sites/data/rs1 --port 27017 --fork
+mongod --replSet rs1 --logpath "2.log" --dbpath ~/Sites/data/rs2 --port 27018 --fork
 mongod --replSet rs1 --logpath "3.log" --dbpath ~/Sites/data/rs3 --port 27019 --fork
