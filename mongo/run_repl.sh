816e7d87cea7f60bc06748cfd7bc828b93081385
admix <admix.snurnikov@gmail.com>
2014-07-12 19:41:35 -0400

    Added mongod replica set example

---
+++ mongo/run_repl.sh
@@ -0,0 +1,3 @@
+mongod --replSet rs1 --logpath "1.log" --dbpath ~/Sites/data/rs1 --port 27019 --fork
+mongod --replSet rs1 --logpath "2.log" --dbpath ~/Sites/data/rs2 --port 27019 --fork
+mongod --replSet rs1 --logpath "3.log" --dbpath ~/Sites/data/rs3 --port 27019 --fork
