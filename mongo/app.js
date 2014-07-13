3a777c890243feaa655b40749339d173286a76ad
admix <admix.snurnikov@gmail.com>
2014-07-12 20:46:56 -0400

    Modified app

--- mongo/app.js
+++ mongo/app.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect("mongodb://localhost:27017,localhost:27018,localhost:27019/course", function(err, db) {
+MongoClient.connect("mongodb://localhost:27018/course,localhost:27017,localhost:27019", function(err, db) {
     if (err) throw err;
 
     var documentNumber = 0;
